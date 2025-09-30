fe10b1984d8746f0980679b823d61e59695a108b
andres.ojeda <andres.ojeda@iesnervion.es>
2025-09-30 14:39:15 +0200

    B2 EJ1 fix

--- TEMA 0/EJERCICIOS/BOLETIN 2/EJ1.py
+++ TEMA 0/EJERCICIOS/BOLETIN 2/EJ1.py
@@ -1,5 +1,9 @@
 #Crea una lista de enteros de longitud 10 que se inicializará con números aleatorios comprendidos entre 1 y 100. 
 
 import random
+
+# Crear y rellenar la lista con números aleatorios entre 1 y 100
 lista = [random.randint(1, 100) for _ in range(10)]
+
+# Mostrar la lista
 print(lista)
